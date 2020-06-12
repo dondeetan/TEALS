b9bc3a738b02bb36088361f7bae5c1d2eeeee330
Don Tan <dondee.tan@alaskaair.com>
2020-06-12 15:53:06 -0700

    Adding new code

--- DotNet/AlgorithmsAndDataStructures.Tests/AlgorithmsTests.cs
+++ DotNet/AlgorithmsAndDataStructures.Tests/AlgorithmsTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using AlgorithmsAndDataStructures.Algorithms;
 using System;
+using System.Linq;
 
 namespace AlgorithmsAndDataStructures.Tests
 {
@@ -14,7 +15,10 @@ namespace AlgorithmsAndDataStructures.Tests
         [Test]
         public void VValidateReversString_Solution1_ReturnsMaxCharacters()
         {
+            //Arrange and Act
             var results = MaxCharacters.FindMaxCharacters("Hello There!");
+
+            //Assert
             Assert.IsTrue(results.Item1 == 'e');
             Assert.IsTrue(results.Item2 == 3);             
         }
@@ -22,26 +26,51 @@ namespace AlgorithmsAndDataStructures.Tests
         [Test]
         public void ValidateReversString_Solution2_ReturnsRevereString()
         {
+            //Arrange and Act
             var results = ReversString.ReverseStringSolution1("apple");
+
+            //Assert
             Assert.IsTrue(results == "elppa");            
         }
 
         [Test]
         public void ValidateReversString_Solution1_ReturnsRevereString()
         {
+            //Arrange and Act
             var results = ReversString.ReverseStringSolution1("apple");
+
+            //Assert
             Assert.IsTrue(results == "elppa");            
         }
 
          [Test]
         public void ValidateFizzBuzz_Solution1_PrintsFizzBuzz()
         {
+            //Arrange and Act
             int[] intarray = new int[50];
             for (int i = 0; i < intarray.Length ; i++)
             {
                 intarray[i] = i + 1;
             }            
             FizzBuzz.FindFizzbuzz(intarray);           
+        }
+
+          [Test]
+        public void ValidateChunkArray_Solution1_ReturnsNChunks()
+        {
+            //Arrange and Act
+            // split on groups with each 100 items
+            String[][] chunks = ChunkArray.ChunkArrays(10);
+
+            for (int i = 0; i < chunks.Length; i++)
+            {
+                foreach (var item in chunks[i])
+                    Console.WriteLine($"chunk:{i} {item}");
+            }     
+
+            //Assert
+            // With 100 items and chunknumber (n) of 10, the result should be 10 chunks
+            Assert.IsTrue(chunks.Length == 10);
         }
     }
 }
