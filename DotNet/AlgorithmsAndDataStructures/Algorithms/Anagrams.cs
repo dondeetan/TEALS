a59592bb594ba0b51617503effee2923cf6bc986
Dondee Tan <Dondee.Tan@live.com>
2020-10-10 07:00:02 -0700

    Refactoring codebase

--- DotNet/AlgorithmsAndDataStructures/Algorithms/Anagrams.cs
+++
@@ -1,8 +0,0 @@
-namespace AlgorithmsAndDataStructures.Algorithms
-{
-    public class Anagrams
-    {
-        //https://stackoverflow.com/questions/32778070/how-can-check-anagram-strings-in-c-sharps
-        public Anagrams(){}
-    }
-}
